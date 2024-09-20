9d674f1c1615d52f5868c30da6677bb7944abc9a
Abhishek Rana <abhishekrana4a0@gmail.com>
2024-09-20 10:35:07 +0530

    event added

---
+++ reactbasics/src/App.js
@@ -0,0 +1,19 @@
+
+import React,{useState} from "react"
+import './App.css';
+
+function App() {
+  const [count,setCount] = useState(0);
+  const handleClick = () =>{
+    setCount(count+1)
+  }
+  return (
+    <div className="App">
+     <h1>Button Click Event</h1>
+     <p>You have clicked {count}!</p>
+     <button onClick={handleClick}>Click Me!</button>
+    </div>
+  );
+}
+
+export default App;
