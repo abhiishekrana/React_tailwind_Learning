a21e943f3f98bf969cb020811189175a4af3c22c
Abhishek Rana <abhishekrana4a0@gmail.com>
2024-09-27 13:02:06 +0530

    episode 4 29.27 completed

---
+++ uiweb/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Navbar.css'
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <h1>LOGO</h1>
+      <ul className='menu_items'>
+        <li>Men</li>
+        <li>Women</li>
+        <li>Kids</li>
+        <li>Cart</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ uiweb/src/components/ProductCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './ProductCard.css'
+import Product from './Product'
+
+const ProductCard = () => {
+  return (
+    <div className='product_card'>
+    <Product/>
+    <Product/>
+    <Product/>
+    <Product/>
+    <Product/>
+    <Product/>
+    <Product/>
+    <Product/>
+    <Product/>
+    </div>
+  )
+}
+
+export default ProductCard
