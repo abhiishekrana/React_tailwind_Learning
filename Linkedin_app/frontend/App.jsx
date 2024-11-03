1f86735a831b459702a69f1e9f300f796cfb43be
Abhishek Rana <abhishekrana4a0@gmail.com>
2024-11-03 20:49:11 +0530

    will start tom

--- Linkedin_app/frontend/App.jsx
+++ Linkedin_app/frontend/App.jsx
@@ -5,6 +5,7 @@ const App = () => {
     <div>
       {/* frontend started */}
       <h1>From tom onwards prepare regriously.</h1>
+      <h2>I am at home will start by tomm</h2>
     </div>
   )
 }
